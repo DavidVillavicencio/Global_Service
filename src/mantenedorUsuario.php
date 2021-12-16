e5f6a64ca8973057bb81105667ebb330a7bcee6c
Kevin Cruz <kevin_mcp@outlook.com>
2021-12-16 14:03:13 -0300

    Validar rut en editar listo

--- src/mantenedorUsuario.php
+++ src/mantenedorUsuario.php
@@ -243,7 +243,7 @@ if (isset($_POST['buscador'])) {
             </div>
             <div class="form-group mx-sm-5 mb-2">
               <label for="inputPassword">Contraseña</label>
-              <input type="password" name="contraseña" class="form-control form-control-sm" id="inputPassword" placeholder="Contraseña" required>
+              <input type="password" name="contraseña" class="form-control form-control-sm pw" id="inputPassword" placeholder="Contraseña" required>
             </div>
           </div>
           <div class="modal-footer">
@@ -266,7 +266,7 @@ if (isset($_POST['buscador'])) {
           <h5 class="modal-title" id="modalLabelEditar">Editar usuario</h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
-        <form method="post" action="../Recursos/Funciones/usuarioFx.php">
+        <form method="post" name="form2" onSubmit="javascript:return Rut(document.form2.rut.value)" action="../Recursos/Funciones/usuarioFx.php">
 
           <div class="modal-body">
 
@@ -293,13 +293,13 @@ if (isset($_POST['buscador'])) {
             </div>
             <div class="form-group mx-sm-5 mb-2">
               <label for="inputPasswordEditar">Contraseña</label>
-              <input type="password" class="form-control form-control-sm" id="inputPasswordEditar" placeholder="Contraseña" name="contraseña" required>
+              <input type="password" class="form-control form-control-sm pw" id="inputPasswordEditar" placeholder="Contraseña" name="contraseña" minlength="8" required>
             </div>
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
             <button type="reset" class="btn btn-secondary">Limpiar</button>
-            <button type="submit" name="modificar" class="btn btn-primary">Editar</button>
+            <button type="submit" name="modificar" class="btn btn-primary" onClick="validarCampos()">Editar</button>
           </div>
         </form>
 
@@ -319,6 +319,7 @@ if (isset($_POST['buscador'])) {
     }
 
     let valor;
+
     function botonEditar(valor) {
 
       let rutEditar = document.querySelector("#inputRutEditar")
@@ -339,7 +340,7 @@ if (isset($_POST['buscador'])) {
 
     }
 
-    // Validador de Rut
+    // Validador de Rut agregar
     function revisarDigito(dvr) {
       dv = dvr + ""
       if (dv != '0' && dv != '1' && dv != '2' && dv != '3' && dv != '4' && dv != '5' && dv != '6' && dv != '7' && dv != '8' && dv != '9' && dv != 'k' && dv != 'K') {
@@ -455,6 +456,131 @@ if (isset($_POST['buscador'])) {
 
       return false;
     }
+
+    //Validador de Rut Editar
+    function revisarDigitoEditar(dvr) {
+      dv = dvr + ""
+      if (dv != '0' && dv != '1' && dv != '2' && dv != '3' && dv != '4' && dv != '5' && dv != '6' && dv != '7' && dv != '8' && dv != '9' && dv != 'k' && dv != 'K') {
+        alert("Debe ingresar un digito verificador valido");
+        window.document.form2.rut.focus();
+        window.document.form2.rut.select();
+        return false;
+      }
+      return true;
+    }
+
+    function revisarDigito2Editar(crut) {
+      largo = crut.length;
+      if (largo < 2) {
+        alert("Debe ingresar el rut completo")
+        window.document.form2.rut.focus();
+        window.document.form2.rut.select();
+        return false;
+      }
+      if (largo > 2)
+        rut = crut.substring(0, largo - 1);
+      else
+        rut = crut.charAt(0);
+      dv = crut.charAt(largo - 1);
+      revisarDigitoEditar(dv);
+
+      if (rut == null || dv == null)
+        return 0
+
+      var dvr = '0'
+      suma = 0
+      mul = 2
+
+      for (i = rut.length - 1; i >= 0; i--) {
+        suma = suma + rut.charAt(i) * mul
+        if (mul == 7)
+          mul = 2
+        else
+          mul++
+      }
+      res = suma % 11
+      if (res == 1)
+        dvr = 'k'
+      else if (res == 0)
+        dvr = '0'
+      else {
+        dvi = 11 - res
+        dvr = dvi + ""
+      }
+      if (dvr != dv.toLowerCase()) {
+        alert("EL rut es incorrecto")
+        window.document.form2.rut.focus();
+        window.document.form2.rut.select();
+        return false
+      }
+
+      return true
+    }
+
+    function Rut(texto) {
+      var tmpstr = "";
+      for (i = 0; i < texto.length; i++)
+        if (texto.charAt(i) != ' ' && texto.charAt(i) != '.' && texto.charAt(i) != '-')
+          tmpstr = tmpstr + texto.charAt(i);
+      texto = tmpstr;
+      largo = texto.length;
+
+      if (largo < 2) {
+        alert("Debe ingresar el rut completo")
+        window.document.form2.rut.focus();
+        window.document.form2.rut.select();
+        return false;
+      }
+
+      for (i = 0; i < largo; i++) {
+        if (texto.charAt(i) != "0" && texto.charAt(i) != "1" && texto.charAt(i) != "2" && texto.charAt(i) != "3" && texto.charAt(i) != "4" && texto.charAt(i) != "5" && texto.charAt(i) != "6" && texto.charAt(i) != "7" && texto.charAt(i) != "8" && texto.charAt(i) != "9" && texto.charAt(i) != "k" && texto.charAt(i) != "K") {
+          alert("El valor ingresado no corresponde a un R.U.T valido");
+          window.document.form2.rut.focus();
+          window.document.form2.rut.select();
+          return false;
+        }
+      }
+
+      var invertido = "";
+      for (i = (largo - 1), j = 0; i >= 0; i--, j++)
+        invertido = invertido + texto.charAt(i);
+      var dtexto = "";
+      dtexto = dtexto + invertido.charAt(0);
+      dtexto = dtexto + '-';
+      cnt = 0;
+
+      for (i = 1, j = 2; i < largo; i++, j++) {
+        //alert("i=[" + i + "] j=[" + j +"]" );		
+        if (cnt == 3) {
+          dtexto = dtexto + '.';
+          j++;
+          dtexto = dtexto + invertido.charAt(i);
+          cnt = 1;
+        } else {
+          dtexto = dtexto + invertido.charAt(i);
+          cnt++;
+        }
+      }
+
+      invertido = "";
+      for (i = (dtexto.length - 1), j = 0; i >= 0; i--, j++)
+        invertido = invertido + dtexto.charAt(i);
+
+      window.document.form2.rut.value = invertido.toUpperCase()
+
+      if (revisarDigito2Editar(texto))
+        return true;
+
+      return false;
+    }
+
+  //Validar password si tiene mayuscula y números
+  function validarCampos() {
+    let campoContraseña = document.querySelector(".pw")
+    console.log(campoContraseña)
+
+  }
+
   </script>
 
 
